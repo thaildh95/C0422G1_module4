e3113c81e44723731d6d2fedbf00a41089058b66
thaildh95 <thaildh.rtbed@gmail.com>
2022-08-26 21:50:11 +0700

    u

--- ss9/exercise/book/src/main/java/com/book/aspect/LibraryAspect.java
+++ ss9/exercise/book/src/main/java/com/book/aspect/LibraryAspect.java
@@ -1,16 +1,24 @@
 package com.book.aspect;
 
-import org.aopalliance.intercept.Joinpoint;
+import org.aspectj.lang.JoinPoint;
 import org.aspectj.lang.annotation.AfterReturning;
 import org.aspectj.lang.annotation.Aspect;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
+
 
 @Aspect
 @Component
 public class LibraryAspect {
 
-@AfterReturning(pointcut = "execution(* com.book.controller.LibraryController.*(..))")
-    public void logInfoProcessing(Joinpoint joinpoint){
-    String 
-}
+    private int count = 1;
+
+    @AfterReturning(pointcut = "execution(* com.book.controller.LibraryController.*(..))", argNames = "joinPoint")
+    public void infoProcessing(JoinPoint joinPoint) {
+      String methodName = joinPoint.getSignature().getName();
+        String args = Arrays.toString(joinPoint.getArgs());
+        System.err.println("Method " + methodName + "đã chạy xong");
+        System.err.println(args);
+    }
 }
--- ss9/exercise/book/src/main/java/com/book/controller/LibraryController.java
+++ ss9/exercise/book/src/main/java/com/book/controller/LibraryController.java
@@ -5,8 +5,8 @@ import com.book.service.ILibraryService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -23,30 +23,46 @@ public class LibraryController {
     }
     @GetMapping("/detail")
     public String goDetail(Model model, @RequestParam int id){
+
         model.addAttribute("book",service.findById(id));
         return "/detail";
     }
     @PostMapping("/borrow")
-    public String borrow(Model model,@RequestParam int id){
+    public String borrow(Model model,@RequestParam int id) throws Exception {
         Library libraries = service.findById(id);
+        if (libraries==null){
+            throw new Exception();
+        }
         if (libraries.getCount()<=0){
-            model.addAttribute("mess1","Hết sách rồi");
+            model.addAttribute("mess","Hết sách rồi");
             return "detail";
         }
         libraries.setCount(libraries.getCount()-1);
         service.save(libraries);
         return "redirect:/";
     }
+    @GetMapping("/pay")
+    public String goPay(){
+        return "/pay";
+    }
 
     @PostMapping("/pay")
-    public String pay(Model model,@RequestParam int id){
+    public String pay(Model model,@RequestParam int id) throws Exception {
         Library libraries = service.findById(id);
-        if (libraries.getCount()>libraries.getTotal()){
-            model.addAttribute("mess1","Quá số lượng sách trả");
-            return "redirect:/";
-        }
+      if (libraries == null){
+          throw  new Exception();
+      }
+      if (libraries.getCount() == libraries.getTotal()){
+          model.addAttribute("mess","số sách đã đủ");
+          return "pay";
+      }
         libraries.setCount(libraries.getCount()+1);
         service.save(libraries);
-        return "redirect:/";
+        model.addAttribute("mess","trả thành công");
+        return "redirect:/home";
+    }
+    @ExceptionHandler(value =  Exception.class)
+    public String errors(){
+        return "errors";
     }
 }
